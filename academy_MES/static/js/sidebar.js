847bc2fd6a2434a21fef5a11835ac600d3a61020
Rom1987 <maks3564195@gmail.com>
2022-05-29 12:13:01 +0300

    make it better

--- academy_MES/static/js/sidebar.js
+++ academy_MES/static/js/sidebar.js
@@ -1,7 +1,15 @@
+// определяем как будет расположен сайдербар в зависимости от ширины экрана
+positionSidebarDependingOnTheWidth()
+
+// если ширина окна изменилась
 window.addEventListener('resize', function (event) {
+    positionSidebarDependingOnTheWidth()
+}, true)
+
+function positionSidebarDependingOnTheWidth() {
     if (document.documentElement.clientWidth < 960) {
         wrapper.insertBefore(aside, main)
     } else {
         main.insertBefore(aside, content)
     }
-}, true)
+}
